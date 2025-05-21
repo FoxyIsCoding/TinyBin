2bfa18c8947d204967d8112d96e5c5cf1ae9df69
FoxyIsCoding <matej.bielik@proton.me>
2025-05-21 18:28:00 +0200

    Refactor database connection and note management; remove load_db.js, update .env, and add db.js

---
+++ app/src/backend/db.js
@@ -0,0 +1,51 @@
+import dotenv from 'dotenv'
+dotenv.config()
+
+import mysql from 'mysql2/promise'
+
+let connection
+
+export async function connectDb() {
+  if (!connection) {
+    connection = await mysql.createConnection({
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+    })
+    console.log('Connected to DB successfully!')
+  }
+  return connection
+}
+
+export async function addNote(title, text, expiresAt = null) {
+  const conn = await connectDb()
+  const [result] = await conn.execute(
+    `INSERT INTO notes (title, text, created_at, expires_at)
+     VALUES (?, ?, CURRENT_TIMESTAMP, ?)`,
+    [title, text, expiresAt]
+  )
+  return result.insertId
+}
+
+
+export async function getNoteById(id) {
+    const conn = await connectDb()
+    const [rows] = await conn.execute(
+        `SELECT id, title, text, created_at, expires_at
+         FROM notes
+         WHERE id = ? AND (expires_at IS NULL OR expires_at > CURRENT_TIMESTAMP)`,
+        [id]
+    )
+    return rows[0]
+}
+
+export async function removeExpiredNotes() {
+    const conn = await connectDb()
+    const [result] = await conn.execute(
+        `DELETE FROM notes
+         WHERE expires_at IS NOT NULL AND expires_at <= CURRENT_TIMESTAMP`
+    )
+    return result.affectedRows
+}
--- app/src/main.js
+++ app/src/main.js
@@ -1,13 +1,23 @@
 import dotenv from 'dotenv';
 dotenv.config()
 
-import { loadDb } from './backend/load_db.js';
+import { connectDb, getNoteById, removeExpiredNotes, addNote } from './backend/db.js';
 
 //setup and load all shits
 async function start() {
   console.log('Starting application...')
   console.log("Starting db")
-  await loadDb()
+  await connectDb()
+  await startupCheck()
+  console.log("Starting web server")
+  // await startWebServer() - TODO: implement this
+}
+
+
+async function startupCheck() {
+  await removeExpiredNotes()
+  console.log("Removed old expired notes")
+
 }
 
 start()
