d35d989d8fa9a942b96dd722c4fd8c92ac12ab62
FoxyIsCoding <matej.bielik@proton.me>
2025-05-26 16:22:26 +0200

    some import fixes

--- backend/src/queries/notes.ts
+++ backend/src/queries/notes.ts
@@ -2,15 +2,15 @@
 Follow a format where you have "queries" functions which interact with the database. No route should ever do DB specific logic on it. It should only be calling the pre made functions
 */
 
-import { db } from '../db/index.js';
+import { pool } from '../database.js';
 import { notes } from '../db/schema.js';
-import { generateNoteId } from '../utils/id';
+import { generateId } from '../utils/id.js';
 import { eq, lt, isNotNull, and } from 'drizzle-orm';
 
 // Export exports it where you can impot via {create_note}
 export async function createNote(title: string, text: string, expiresAt: Date | null = null): Promise<string> {
-      const url = await generateNoteId();
-      const inserted = await db.insert(notes).values({
+      const url = await generateId();
+      const inserted = await pool.insert(notes).values({
         title,
         text,
         expiresAt,
@@ -22,17 +22,17 @@ export async function createNote(title: string, text: string, expiresAt: Date | 
 };
 
 export async function getNoteById(id: string) {
-  const result = await db.select().from(notes).where(eq(notes.id, parseInt(id)));
+  const result = await pool.select().from(notes).where(eq(notes.id, parseInt(id)));
   return result[0];
 }
 
 export async function getNoteByUrl(url: string) {
-  const result = await db.select().from(notes).where(eq(notes.url, url));
+  const result = await pool.select().from(notes).where(eq(notes.url, url));
   return result[0];
 }
 
 export async function removeExpiredNotes() {
-  const result = await db.delete(notes).where(
+  const result = await pool.delete(notes).where(
     and(isNotNull(notes.expiresAt), lt(notes.expiresAt, new Date()))
   );
   return result.rowCount ?? 0;
