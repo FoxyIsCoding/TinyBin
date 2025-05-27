9ff864f3a3c915b8e1a688546b078e95882bdce3
FoxyIsCoding <77046186+FoxyIsCoding@users.noreply.github.com>
2025-05-27 21:09:25 +0200

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -9,11 +9,16 @@ export default defineConfig({
 				target: 'http://localhost:3000',
 				changeOrigin: true
 			}
-		}
+		},
+		host: true, // allows access from external hosts
+		origin: 'https://tinybin.fun' // sets the origin for HMR & stuff (optional but nice)
 	},
 	preview: {
-		host: "tinybin.fun",
+		host: "0.0.0.0",
 		port: 4173,
-		strictPort: true
+		strictPort: true,
+		headers: {
+			'Access-Control-Allow-Origin': 'https://tinybin.fun'
+		}
 	}
 });
